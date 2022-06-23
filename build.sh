9cff57193bfbfe22759fd0f3b76875d46420c4da
msjae <mugun19@gmail.com>
2022-06-23 12:59:25 +0900

    build.sh

--- build.sh
+++ build.sh
@@ -17,40 +17,40 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 sudo make install
 
-cd ../../g2o
+
 
 echo "Configuring and building Thirdparty/g2o ..."
-
+cd ../../g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j$nproc
 sudo make install
 
-cd ../../Pangolin
+
 
 echo "Configuring and building Thirdparty/Pangolin ..."
-
+cd ../../Pangolin
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j$nproc
 sudo make install
 
-cd ../../easy_profiler
+
 
 echo "Configuring and building Thirdparty/easy_profiler ..."
-
+cd ../../easy_profiler
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j$nproc
 sudo make install
 
-cd ../../opencv
+
 
 echo "Configuring and building Thirdparty/opencv ..."
-
+cd ../../opencv
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release \
@@ -63,17 +63,17 @@ cmake .. -DCMAKE_BUILD_TYPE=Release \
 make -j$nproc
 sudo make install
 
-cd ../../../
+
 
 
 echo "Uncompress vocabulary ..."
-
+cd ../../../
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
-cd ..
+
 
 echo "Configuring and building ORB_SLAM2 ..."
-
+cd ..
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
