94d007af19ed473b192088d04a3cd0c1120d6776
msjae <mugun19@gmail.com>
2022-06-23 15:24:15 +0900

    pangolin 0.5

--- build.sh
+++ build.sh
@@ -49,19 +49,6 @@ sudo make install
 
 
 
-echo "Configuring and building Thirdparty/opencv ..."
-cd ../../opencv
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_C_EXAMPLES=ON \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D OPENCV_GENERATE_PKGCONFIG=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules \
-    -D BUILD_EXAMPLES=ON ..
-make -j$nproc
-sudo make install
 
 
 
